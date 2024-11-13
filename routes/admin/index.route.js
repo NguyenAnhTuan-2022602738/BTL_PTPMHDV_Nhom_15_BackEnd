13ae73ddc71b10fc4d0dd6f899d1b527c6b07d92
Nguyễn Anh Tuấn <tuannguyen10112004@gmail.com>
2024-11-13 20:52:26 +0700

    tính năng chỉnh sửa nhóm quyền

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -2,11 +2,13 @@ const dashboardRouters = require("./dashboard.route");
 const systemAdmin = require("../../config/system");
 const productRouters = require("./product.route");
 const productCategoryRouters = require("./product-category.route");
+const roleRouters = require("./role.route");
 
 module.exports = (app) => {
-    const PATH_ADMIN = systemAdmin.prefixAdmin;
-    
-    app.use(PATH_ADMIN + "/dashboard", dashboardRouters);
-    app.use(PATH_ADMIN + "/car_items", productRouters);
-    app.use(PATH_ADMIN + "/car_items-category", productCategoryRouters);
-}
+  const PATH_ADMIN = systemAdmin.prefixAdmin;
+
+  app.use(PATH_ADMIN + "/dashboard", dashboardRouters);
+  app.use(PATH_ADMIN + "/car_items", productRouters);
+  app.use(PATH_ADMIN + "/car_items-category", productCategoryRouters);
+  app.use(PATH_ADMIN + "/roles", roleRouters);
+};
